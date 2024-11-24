f336e4b5677405a26842b4060cc2d784e167aed8
soniasabherwal <106363909+soniasabherwal@users.noreply.github.com>
2024-11-23 20:39:14 -0800

    Update script.js

--- Frontend folder/scr/script.js
+++ Frontend folder/scr/script.js
@@ -1,1 +1,13 @@
 
+function fetchData(endpoint) {
+    fetch(`http://127.0.0.1:5000/${endpoint}`)
+        .then(response => response.json())
+        .then(data => {
+            const dataDiv = document.getElementById("data");
+            dataDiv.innerHTML = `<h2>${endpoint.charAt(0).toUpperCase() + endpoint.slice(1)}</h2>`;
+            data.forEach(item => {
+                dataDiv.innerHTML += `<pre>${JSON.stringify(item, null, 2)}</pre>`;
+            });
+        })
+        .catch(err => console.error(err));
+}
